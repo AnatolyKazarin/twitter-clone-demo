2c60cd315a7c0eac3dafe76173c0022b7d4959d5
AnatolyKazarin <alkidkaz@gmail.com>
2023-05-14 23:23:00 +0400

    Profile Banner Animations

---
+++ Twitter-Clone/View/Profile/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  Twitter-Clone
+//
+//  Created by Анатолий on 14.05.2023.
+//
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemChromeMaterialDark))
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
