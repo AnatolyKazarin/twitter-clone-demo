0c379b05231ff799625a02c30873d934b94becea
AnatolyKazarin <alkidkaz@gmail.com>
2023-05-15 11:08:43 +0400

    Profile Bio

--- Twitter-Clone/View/Profile/UserProfile.swift
+++ Twitter-Clone/View/Profile/UserProfile.swift
@@ -27,7 +27,7 @@ struct UserProfile: View {
                             Image("logo")
                                 .resizable()
                                 .aspectRatio(contentMode: .fill)
-                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 0 : 180 + minY, alignment: .center)
+                                .frame(width: UIScreen.main.bounds.width, height: minY > 0 ? 180 + minY : 180, alignment: .center)
                                 .cornerRadius(0)
                             
                             BlurView()
@@ -52,6 +52,62 @@ struct UserProfile: View {
                 }
                 .frame(height: 180)
                 .zIndex(1)
+                
+                VStack {
+                    HStack {
+                        
+                        Image("logo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 75, height: 75)
+                            .clipShape(Circle())
+                            .padding(8)
+                            .background(Color.white)
+                            .offset(y: offset < 0 ? getOffset() - 20 : -20)
+                            .scaleEffect(getScale())
+                        
+                        Spacer()
+                        
+                        Button(action: {}, label: {
+                            Text("Edit Profile")
+                                .foregroundColor(.blue)
+                                .padding(.vertical, 10)
+                                .padding(.horizontal)
+                                .background(Capsule().stroke(Color.blue, lineWidth: 1.5))
+                        })
+                    }
+                    .padding(.top, -25)
+                    .padding(.bottom, -10)
+                    
+                    VStack(alignment: .leading, spacing: 8, content: {
+                        Text("Anatoly")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.primary)
+                        
+                        Text("@alkid")
+                            .foregroundColor(.gray)
+                        
+                        Text("Profile Description")
+                        
+                        HStack(spacing: 5, content: {
+                            Text("13")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                            
+                            Text("Followers")
+                                .foregroundColor(.gray)
+                            
+                            Text("680")
+                                .foregroundColor(.primary)
+                                .fontWeight(.semibold)
+                                .padding(.leading, 10)
+                            
+                            Text("Following")
+                                .foregroundColor(.gray)
+                        })
+                    })
+                }
             }
         }
     }
@@ -65,6 +121,17 @@ struct UserProfile: View {
         let progress = 20 / titleOffset
         let offset = 60 * (progress > 0 && progress <= 1 ? progress : 1)
         return offset
+    }
+    
+    func getOffset() -> CGFloat {
+        let progress = (-offset / 80) * 20
+        return progress <= 20 ? progress : 20
+    }
+    
+    func getScale() -> CGFloat {
+        let progress = -offset / 80
+        let scale = 1.8 - (progress < 1.0 ? progress : 1)
+        return scale < 1 ? scale: 1
     }
 }
 
